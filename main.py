bb6a4ee7fddb1e6afbee7ece13775a15afedc232
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2024-10-08 17:23:47 +0200

    Hide socket mode behind dev mode env var

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+logging.basicConfig(level=logging.DEBUG)
+
 load_dotenv()
 
-# Initialization
-logging.basicConfig(level=logging.DEBUG)
+dev_mode_enabled = bool(os.environ.get("DEV_MODE_ENABLED", False))
+port = int(os.environ.get("PORT", 8080))
+
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# register_listeners(app)
-
 # Listens to incoming messages that contain "hello"
-# To learn available listener arguments,
-# visit https://tools.slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
 @app.message("hello")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
-# Start Bolt app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+    if dev_mode_enabled:
+        SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+    else:
+        app.start(port=port)
