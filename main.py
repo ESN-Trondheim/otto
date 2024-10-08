b63fd2bb153a14954aebac4d5c777bf2bbfaadca
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2024-10-08 20:28:08 +0200

    Use Flask wrapper

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import logging
 
 from dotenv import load_dotenv
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-logging.basicConfig(level=logging.DEBUG)
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request
 
 load_dotenv()
-
-dev_mode_enabled = bool(os.environ.get("DEV_MODE_ENABLED", False))
-port = int(os.environ.get("PORT", 8080))
+logging.basicConfig(level=logging.DEBUG)
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
@@ -23,8 +20,13 @@ def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
+api = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@api.route("/slack/events", methods=["POST"])
+def slack_events():
+    """This is the main entry point of Slack events into the application"""
+    return handler.handle(request)
+
 if __name__ == "__main__":
-    if dev_mode_enabled:
-        SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
-    else:
-        app.start(port=port)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
