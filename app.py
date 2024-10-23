45bab1ad36d70fb05889dd5110068ca3a20fcbee
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2024-10-23 21:38:22 +0200

    Refactor files and add assets

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-
-from event_handlers import register_event_handlers
+from slack_sdk import WebClient
 
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
@@ -19,7 +18,26 @@ app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"), signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-register_event_handlers(app)
+
+@app.event("assistant_thread_started")
+def assistant_thread_started(event, client: WebClient):
+    """Handles the assistant_thread_started event: https://api.slack.com/events/assistant_thread_started"""
+    thread = event["assistant_thread"]
+    client.chat_postMessage(
+        channel=thread["channel_id"],
+        thread_ts=thread["thread_ts"],
+        text=f"Hey there <@{thread["user_id"]}>! How can I help you today?",
+    )
+
+
+@app.event("message")
+def message(event, client: WebClient):
+    """Handles the message event: https://api.slack.com/events/message.im"""
+    client.chat_postMessage(
+        channel=event["channel"],
+        thread_ts=event["thread_ts"],
+        text=f"<@{event["user"]}> said: '{event["text"]}'",
+    )
 
 
 @app.middleware
@@ -30,13 +48,13 @@ def log_request(logger, body, next):
 
 # Slack Bolt App is wrapped in a Flask adapter, and Flask API is used to handle incoming event requests.
 api = Flask(__name__)
-handler = SlackRequestHandler(app)
+slack_handler = SlackRequestHandler(app)
 
 
 @api.route("/slack/events", methods=["POST"])
 def slack_events():
     """This is the main entry point of Slack events into the application"""
-    return handler.handle(request)
+    return slack_handler.handle(request)
 
 
 # When this file is executed directly the API is served.
