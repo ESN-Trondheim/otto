a9ba028ab84dd5afd3dd5397cc77e401db54183f
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2024-10-23 20:30:36 +0200

    Add lint job to project

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
 import logging
+import os
 
 from dotenv import load_dotenv
+from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-from flask import Flask, request
 
 from event_handlers import register_event_handlers
 
@@ -16,27 +16,31 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Slack Bolt App is created and event handler functions are registered.
 app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+    token=os.environ.get("SLACK_BOT_TOKEN"), signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
 register_event_handlers(app)
 
-@app.middleware 
+
+@app.middleware
 def log_request(logger, body, next):
     logger.debug(body)
     return next()
 
+
 # Slack Bolt App is wrapped in a Flask adapter, and Flask API is used to handle incoming event requests.
 api = Flask(__name__)
 handler = SlackRequestHandler(app)
+
 
 @api.route("/slack/events", methods=["POST"])
 def slack_events():
     """This is the main entry point of Slack events into the application"""
     return handler.handle(request)
 
+
 # When this file is executed directly the API is served.
 if __name__ == "__main__":
     from waitress import serve
+
     serve(api, host="0.0.0.0", port=os.environ.get("PORT", 8080))
--- event_handlers.py
+++ event_handlers.py
@@ -3,22 +3,23 @@ from slack_sdk import WebClient
 
 # This file contains event handlers for the Slack Bolt app
 
+
 def register_event_handlers(app: App):
     @app.event("assistant_thread_started")
     def assistant_thread_started(event, client: WebClient):
         """Handles the assistant_thread_started event: https://api.slack.com/events/assistant_thread_started"""
         thread = event["assistant_thread"]
         client.chat_postMessage(
-            channel=thread["channel_id"], 
+            channel=thread["channel_id"],
             thread_ts=thread["thread_ts"],
-            text=f"Hey there <@{thread["user_id"]}>! How can I help you today?"
+            text=f"Hey there <@{thread["user_id"]}>! How can I help you today?",
         )
 
     @app.event("message")
     def message(event, client: WebClient):
         """Handles the message event: https://api.slack.com/events/message.im"""
         client.chat_postMessage(
-            channel=event["channel"], 
+            channel=event["channel"],
             thread_ts=event["thread_ts"],
-            text=f"<@{event["user"]}> said: '{event["text"]}'"
+            text=f"<@{event["user"]}> said: '{event["text"]}'",
         )
