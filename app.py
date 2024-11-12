552ae5eefef013d78973befee521383aadd66b59
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2024-11-12 18:33:43 +0100

    Refactor bootstrap code

--- app.py
+++ app.py
@@ -47,22 +47,20 @@ def log_request(logger, body, next):
     return next()
 
 
-# Slack Bolt App is wrapped in a Flask adapter, and Flask API is used to handle incoming event requests.
-api = Flask(__name__)
-slack_handler = SlackRequestHandler(app)
-
-
-@api.route("/slack/events", methods=["POST"])
-def slack_events():
-    """This is the main entry point of Slack events into the application"""
-    return slack_handler.handle(request)
-
-
 # When this file is executed directly the API is served either by opening a websocket connection to Slack for local development, or by exposing the Flask API.
 if __name__ == "__main__":
     if os.environ.get("SOCKET_MODE") == "True":
         logging.info("Starting app in socket mode...")
-        SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+        slack_request_handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+        slack_request_handler.start()
     else:
         logging.info("Starting app in API mode...")
+        slack_request_handler = SlackRequestHandler(app)
+        api = Flask(__name__)
+
+        @api.route("/slack/events", methods=["POST"])
+        def slack_events():
+            """This is the main entry point of Slack events into the application"""
+            return slack_request_handler.handle(request)
+        
         serve(api, host="0.0.0.0", port=os.environ.get("PORT", 8080))
