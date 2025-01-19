f2f50fcc673bb8c50aa5d5fa5e4eb72784c50fc7
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2025-01-19 10:10:20 +0100

    Refactor coverimage generator

--- otto/features/coverimage/generator.py
+++ otto/features/coverimage/generator.py
@@ -6,64 +6,75 @@ from PIL import Image, ImageDraw, ImageFont
 from otto.utils.esn import EsnColor
 
 
-class CoverImageFormat(Enum):
-    ESN_ACTIVITIES = "Activities"
-    FACEBOOK = "Facebook"
-
-    @staticmethod
-    def from_value(value: str):
-        for f in CoverImageFormat:
-            if f.value == value:
-                return f
-
-
-class CoverImageDimension(Enum):
-    ESN_ACTIVITIES = (1920, 460)
-    FACEBOOK = (1568, 588)
-
-    def size(self) -> tuple[int]:
-        return self.value
-
-    def width(self) -> int:
-        return self.value[0]
-
-    def height(self) -> int:
-        return self.value[1]
-
-
-class CoverImageOverlay(Enum):
-    ESN_ACTIVITIES = Image.open(
-        Path.cwd().joinpath("assets", "overlays", "coverimage-activities.png")
-    )
-    FACEBOOK = Image.open(
-        Path.cwd().joinpath("assets", "overlays", "coverimage-facebook.png")
-    )
-
-
-class CoverImageTextOffsets(Enum):
-    ESN_ACTIVITIES = (19, -6, 116, 178)
-    FACEBOOK = (19, -6, 90, 152)
-
-    def solotitle_offset(self) -> int:
-        return self.value[0]
-
-    def title_offset(self) -> int:
-        return self.value[1]
-
-    def subtitle_offset(self) -> int:
-        return self.value[2]
-
-    def subsubtitle_offset(self) -> int:
-        return self.value[3]
-
-
-class CoverImageTextFont(Enum):
+class CoverImageFont(Enum):
     TITLE = ImageFont.truetype(
         str(Path.cwd().joinpath("assets", "fonts", "kelson-sans-bold.otf")), 90
     )
     SUBTITLE = ImageFont.truetype(
         str(Path.cwd().joinpath("assets", "fonts", "kelson-sans-bold.otf")), 50
     )
+
+
+class CoverImageFormat(Enum):
+    ESN_ACTIVITIES = (
+        "Activities",
+        (1920, 460),
+        (19, -6, 116, 178),
+        Image.open(
+            Path.cwd().joinpath("assets", "overlays", "coverimage-activities.png")
+        ),
+    )
+    FACEBOOK = (
+        "Facebook",
+        (1568, 588),
+        (19, -6, 90, 152),
+        Image.open(
+            Path.cwd().joinpath("assets", "overlays", "coverimage-facebook.png")
+        ),
+    )
+
+    def __new__(
+        cls, value: str, size: tuple[int], offset: tuple[int], overlay: Image.Image
+    ):
+        """This override allows multi-value enum objects"""
+        obj = object.__new__(cls)
+
+        obj._value_ = value
+        obj.size = size
+        obj.overlay = overlay
+        obj.offset = offset
+
+        return obj
+
+    @property
+    def target_width(self) -> tuple:
+        return self.size[0]
+
+    @property
+    def target_height(self) -> tuple:
+        return self.size[1]
+
+    @property
+    def solotitle_offset(self) -> int:
+        return self.offset[0]
+
+    @property
+    def title_offset(self) -> int:
+        return self.offset[1]
+
+    @property
+    def subtitle_offset(self) -> int:
+        return self.offset[2]
+
+    @property
+    def subsubtitle_offset(self) -> int:
+        return self.offset[3]
+
+    @staticmethod
+    def from_value(value: str):
+        for f in CoverImageFormat:
+            if f.value == value:
+                return f
 
 
 default_subtitle = None
@@ -84,56 +95,57 @@ def create_cover_image(
     background: Image.Image = default_background,
 ):
     cover = background.convert(mode="RGB")
-
-    dimension = CoverImageDimension[format.name]
-    cover = resize_and_crop_to_dimension(cover, dimension)
-
+    cover = resize_and_crop_to_dimension(
+        cover, format.target_width, format.target_height
+    )
     cover = add_color_layer(cover, color)
+    cover = add_overlay_layer(cover, format.overlay)
 
-    overlay = CoverImageOverlay[format.name]
-    cover = add_overlay_layer(cover, overlay)
-
-    text_offsets = CoverImageTextOffsets[format.name]
-    cover = add_text_layer(cover, text_offsets, title, subtitle, subsubtitle)
+    cover = add_text_layer(
+        cover,
+        format.solotitle_offset,
+        format.title_offset,
+        format.subtitle_offset,
+        format.subsubtitle_offset,
+        title,
+        subtitle,
+        subsubtitle,
+    )
 
     return cover
 
 
 def resize_and_crop_to_dimension(
-    image: Image.Image, target_dimension: CoverImageDimension
+    image: Image.Image, target_width: int, target_height: int
 ):
-    if image.size == target_dimension.size():
+    if image.height == target_height and image.width == target_width:
         return image
 
     background_aspect_ratio = image.width / image.height
-    target_aspect_ratio = target_dimension.width() / target_dimension.height()
+    target_aspect_ratio = target_width / target_height
 
     # If background height is too big
     if background_aspect_ratio <= target_aspect_ratio:
         # Resize the image so the height reaches the target width (while maintaining original aspect ratio)
-        resize_ratio = image.width / target_dimension.width()
+        resize_ratio = image.width / target_width
         new_height = int(image.height / resize_ratio)
-        image = image.resize(
-            (target_dimension.width(), new_height), Image.Resampling.LANCZOS
-        )
+        image = image.resize((target_width, new_height), Image.Resampling.LANCZOS)
 
         # Crop away the excessive height if neccessary
-        if image.height > target_dimension.height():
-            padding = (image.height - target_dimension.height()) / 2
-            coords = (0, padding, target_dimension.width(), image.height - padding)
+        if image.height > target_height:
+            padding = (image.height - target_height) / 2
+            coords = (0, padding, target_width, image.height - padding)
             image = image.crop(coords)
     else:  # If background width is too big
         # Resize the image so the height reaches the target height (while maintaining original aspect ratio)
-        resize_ratio = image.height / target_dimension.height()
+        resize_ratio = image.height / target_height
         new_width = int(image.width / resize_ratio)
-        image = image.resize(
-            (new_width, target_dimension.height()), Image.Resampling.LANCZOS
-        )
+        image = image.resize((new_width, target_height), Image.Resampling.LANCZOS)
 
         # Crop away the excessive width if neccessary
-        if image.width > target_dimension.width():
-            padding = (image.width - target_dimension.width()) / 2
-            coords = (padding, 0, image.width - padding, target_dimension.height())
+        if image.width > target_width:
+            padding = (image.width - target_width) / 2
+            coords = (padding, 0, image.width - padding, target_height)
             image = image.crop(coords)
 
     return image
@@ -141,73 +153,51 @@ def resize_and_crop_to_dimension(
 
 def add_color_layer(image: Image.Image, color: EsnColor):
     color_layer = Image.new(image.mode, image.size, color.rgb())
-
     return Image.blend(image, color_layer, 0.60)
 
 
-def add_overlay_layer(image: Image.Image, overlay: CoverImageOverlay):
-    image.paste(overlay.value, (0, 0), overlay.value)
+def add_overlay_layer(image: Image.Image, overlay: Image.Image):
+    image.paste(overlay, (0, 0), overlay)
     return image
 
 
 def add_text_layer(
     image: Image.Image,
-    text_offsets: CoverImageTextOffsets,
+    solo_title_offset: int,
+    title_offset: int,
+    subtitle_offset: int,
+    subsubtitle_offset: int,
     title: str,
     subtitle: str,
     subsubtitle: str,
 ):
     if subtitle or subsubtitle:
-        image = add_title(image, text_offsets, title)
+        image = add_text(image, CoverImageFont.TITLE, title_offset, title)
         if subtitle:
-            image = add_subtitle(image, text_offsets, subtitle)
+            image = add_text(image, CoverImageFont.SUBTITLE, subtitle_offset, subtitle)
         if subsubtitle:
-            image = add_subsubtitle(image, text_offsets, subsubtitle)
+            image = add_text(
+                image,
+                CoverImageFont.SUBTITLE,
+                subsubtitle_offset,
+                subsubtitle,
+            )
     else:
-        image = add_solo_title(image, text_offsets, title)
+        image = add_text(image, CoverImageFont.TITLE, solo_title_offset, title)
 
     return image
 
 
-def add_title(image: Image.Image, text_offsets: CoverImageTextOffsets, text: str):
-    return add_text(
-        image, CoverImageTextFont.TITLE.value, text_offsets.title_offset(), text
-    )
-
-
-def add_subtitle(image: Image.Image, text_offsets: CoverImageTextOffsets, text: str):
-    return add_text(
-        image, CoverImageTextFont.SUBTITLE.value, text_offsets.subtitle_offset(), text
-    )
-
-
-def add_subsubtitle(image: Image.Image, text_offsets: CoverImageTextOffsets, text: str):
-    return add_text(
-        image,
-        CoverImageTextFont.SUBTITLE.value,
-        text_offsets.subsubtitle_offset(),
-        text,
-    )
-
-
-def add_solo_title(image: Image.Image, text_offsets: CoverImageTextOffsets, text: str):
-    return add_text(
-        image, CoverImageTextFont.TITLE.value, text_offsets.solotitle_offset(), text
-    )
-
-
-def add_text(
-    image: Image.Image, font: ImageFont.FreeTypeFont, text_offset: int, text: str
-):
+def add_text(image: Image.Image, font: CoverImageFont, text_offset: int, text: str):
     draw = ImageDraw.Draw(image)
 
-    left, top, right, bottom = draw.textbbox((0, 0), text=text, font=font)
+    left, top, right, bottom = draw.textbbox((0, 0), text=text, font=font.value)
     width = right - left
     height = bottom - top
     draw.text(
         ((image.size[0] - width) / 2, (image.size[1] - height) / 2 + text_offset),
         text,
-        font=font,
+        font=font.value,
     )
 
     return image
